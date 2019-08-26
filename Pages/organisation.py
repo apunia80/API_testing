5c33bfaca177dcd510e9756c1d9377f156f08717
ankit.poonia <ankit.poonia@hashedin.com>
2019-08-26 16:15:47 +0530

    updated functions of organisation

--- Pages/organisation.py
+++ Pages/organisation.py
@@ -8,35 +8,38 @@ class Organisation:
     def __init__(self):
         pass
 
-    def create_organisation(self, query):
+    def create_organisation(self, query):  # 1st API POST
+
         response = requests.request('POST', constant.post_organisation_url,
                                     params=query)
 
         return response
 
     def get_oragnistion_id(self, response):
-        resp = json.loads(response.text)
-        organisation_id = resp['id']
-        return organisation_id
+        org_id = json.loads(response.text)
+        return org_id['id']
 
     def get_organisation_name(self, response):
         resp = json.loads(response.text)
-        displayname = resp['displayName']
-        return displayname
+        return resp['displayName']
 
     def get_organisation_status_code(self, response):
-        statusid = response.status_code
-        return statusid
+        return response.status_code
 
-    def update_the_name_of_organisation(self, organisation_id):
+    def get_api_in_gets_the_status_code(self, organisation_id):     ##get api
+        getresponse = requests.request('GET', constant.post_organisation_url + "/" + organisation_id,
+                                       params={"key": constant.consumer_Key, "token": constant.access_Token})
+        getresponse.status_code
+
+    def update_the_name_of_organisation(self, organisation_id):  ##2nd api PUT
         updatedresponse = requests.request('PUT', constant.post_organisation_url + "/" + organisation_id,
-                                           params={"displayName": "happyshappy", "key": constant.consumer_Key,
+                                           params={"displayName": constant.updated_name, "key": constant.consumer_Key,
                                                    "token": constant.access_Token})
-        resp = json.loads(updatedresponse.text)
-        updateddisplayname = resp['displayName']
-        return updateddisplayname
+        updateddisplayname = json.loads(updatedresponse.text)
 
-    def delete_organisation(self, organisation_id):
+        return updateddisplayname['displayName']
+
+    def delete_organisation(self, organisation_id):  # 4rd api Delete
         res1 = requests.request('DELETE', constant.post_organisation_url + "/" + organisation_id,
                                 params={"key": constant.consumer_Key, "token": constant.access_Token}
                                 )

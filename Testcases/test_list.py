418505ebaa5714480662d4cae714a5a00ecb02fb
ankit.poonia <ankit.poonia@hashedin.com>
2019-08-27 14:46:51 +0530

    testcases addes and fixture applied

--- Testcases/test_list.py
+++ Testcases/test_list.py
@@ -5,7 +5,7 @@ import constant
 from Pages.board import Board
 from Pages.lists import Lists
 
-logging.basicConfig(filename='testloggers.log', format='%(asctime)s :%(levelname)s: %(message)s')
+logging.basicConfig(filename=constant.logger_file, format=constant.logger_format)
 
 
 class TestLists:
@@ -16,8 +16,9 @@ class TestLists:
         self.objorganisation = Organisation()
         self.org_id = self.objlist.organisation()
         self.board_id = self.objlist.board(self.org_id)
-        
+
         self.list_response = self.objlist.create_list_using_post_api(self.board_id)
+        self.list_id = self.objlist.list_id(self.list_response)
         yield
 
         self.objboard.delete_board(self.board_id)
@@ -44,4 +45,66 @@ class TestLists:
         try:
             assert negative_list_response == 400
         except:
-            logging.error("it runs with blank name input")
+            logging.error(" in list it runs with blank name input")
+
+    @pytest.mark.usefixtures('list_fixture')
+    def test_check_position_is_changed_or_not(self):
+        changed_pos = self.objlist.create_list_using_post_api(self.list_id)
+        actual_pos = self.objlist.position_of_the_list(self.list_response)
+        try:
+            assert changed_pos != actual_pos
+        except:
+            logging.error("in list position not changed")
+
+    @pytest.mark.usefixtures('list_fixture')
+    def test_name_is_updated_or_not(self):
+        updated_name = self.objlist.updated_name_of_the_list(self.list_id)
+        actual_name = self.objlist.list_name(self.list_response)
+        try:
+            assert updated_name != actual_name
+        except:
+            logging.error("list name not updated")
+
+    @pytest.mark.usefixtures('list_fixture')
+    def test_put_close_archive_the_list(self):
+        try:
+            assert self.objlist.close_list_by_putclose_api(self.list_id) == True
+        except:
+            logging.error("in list request not archived")
+
+    @pytest.mark.usefixtures('list_fixture')
+    def test_softlimit_status_code_run_or_not_running(self):
+        try:
+            assert self.objlist.set_soft_limit_of_card(self.list_id)
+        except:
+            logging.error("in list softlimit cannot be updated")
+
+    @pytest.mark.usefixtures('list_fixture')
+    def test_subscription_api_status_code(self):
+        try:
+            assert self.objlist.subscription_detail(self.list_id) == 200
+        except:
+            logging.error("in list subscription not done")
+
+    @pytest.mark.usefixtures('list_fixture')
+    def test_subscription_boolien_value_the_list_is_subscribed_or_not(self):
+        try:
+            assert self.objlist.get_api_to_check_sybscription_in_field(self.list_id)==True
+        except:
+            logging.error("in list channel is not subscribed")
+
+    @pytest.mark.usefixtures('list_fixture')
+    def test_get_board_gives_information_about_board_in_which_list_is_present(self):
+        try:
+            assert self.objlist.get_api_check_board_information(self.list_id) == 200
+        except:
+            logging.error("information cannot be fatched")
+
+    @pytest.mark.usefixtures('list_fixture')
+    def test_get_all_information_about_cards(self):
+        try:
+            assert self.objlist.get_list_of_card_in_a_list(self.list_id)
+
+        except:
+            logging.error("list card information cannot fetched")
+
